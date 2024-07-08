3bb175df6206f83af8801cfbd5e935e2ffa251f1
Mutesi-alin <alinemutesiii@gmail.com>
2024-07-08 09:47:26 +0300

    add files

--- app/src/main/java/com/example/payment/MainActivity.kt
+++ app/src/main/java/com/example/payment/MainActivity.kt
@@ -5,16 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.payment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+
+        val transaction= listOf(
+            Transaction("Salary", 40000, "1 July 2024", true),
+            Transaction("Rent", 16000, "2 July 2024", false),
+            Transaction("Dividends", 2400, "4 July 2024", true),
+            Transaction("Electricity", 800, "5 July 2024", false),
+            Transaction("Internet", 2500, "5 July 2024", false),
+            Transaction("Shopping", 3500, "5 July 2024", false),
+            Transaction("Bus fare", 500, "11 July 2024", false)
+        )
+        findViewById<RecyclerView>(R.id.transactionRecyclerView).adapter=
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
